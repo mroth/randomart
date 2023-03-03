538329d0c1ebc194f36bd199682d893bdc7b5ce2
Matthew Rothenberg <mrothenberg@gmail.com>
2023-03-03 13:04:23 -0700

    docs diagram for fc address space

--- example/fcaddr/fcaddr.go
+++ example/fcaddr/fcaddr.go
@@ -11,20 +11,35 @@ import (
 	"github.com/mroth/randomart"
 )
 
-// https://spec.filecoin.io/appendix/address/
-
-// uses lowercase version of standard RFC-4648 base32 encoding
-var low32 = base32.NewEncoding("abcdefghijklmnopqrstuvwxyz234567").WithPadding(base32.NoPadding)
-
 var (
 	tiles  = flag.String("tiles", "galaxy", "galaxy|openssh")
 	width  = flag.Uint("x", 10, "width (cols)")
 	height = flag.Uint("y", 10, "height (rows)")
 )
 
+// Protocol 1 addresses represent secp256k1 public encryption keys. The payload
+// field contains the Blake2b-160 hash of the uncompressed public key (65 bytes).
+//
+// +------------+----------+----------------------------------------------------+
+// |  network   | protocol |             base32 encoded data [39 bytes]         |
+// |------------|----------|----------------------------------------------------|
+// | 'f' or 't' |    '1'   |       'neiyfto7ozo4xaamg35jig7xbbrdpl6s7u6uy4i'    |
+// +------------+----------|--------------------------------+-------------------|
+//                         |              payload           |     checksum      |
+//                         |             [20 byte]          |     [4 byte]      |
+//                         |--------------------------------|-------------------|
+//                         |        blake2b-160(PubKey)     |  blake2b-32(xxx)  |
+//                         +--------------------------------+-------------------+
+//
+// Diagram based on https://spec.filecoin.io/appendix/address/, but clarified.
+
+// uses lowercase version of standard RFC-4648 base32 encoding
+var low32 = base32.
+	NewEncoding("abcdefghijklmnopqrstuvwxyz234567").
+	WithPadding(base32.NoPadding)
+
 func main() {
 	flag.Parse()
-
 	if len(flag.Args()) != 1 {
 		fmt.Fprintln(os.Stderr, "usage: fcaddr <addr>")
 		os.Exit(1)
@@ -32,7 +47,7 @@ func main() {
 	addr := flag.Arg(0)
 
 	if !strings.HasPrefix(addr, "f1") {
-		fmt.Fprintln(os.Stderr, "only f1 address supported")
+		fmt.Fprintln(os.Stderr, "only f1 addresses supported for this demo")
 		os.Exit(3)
 	}
 
